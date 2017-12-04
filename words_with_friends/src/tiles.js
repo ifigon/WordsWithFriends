6c6ba958738ef460e5886bf555e193366edc60e7
Ian Figon <ifigon@outlook.com>
2017-12-04 12:20:34 -0800

    Added removing tile from inventory after placing

--- words_with_friends/src/tiles.js
+++ words_with_friends/src/tiles.js
@@ -3,235 +3,293 @@ export default{
     tile : [
     {
         letter: "A",
-        point: 1
+        point: 1,
+        key: 0
     },
     {
         letter: "A",
-        point: 1
+        point: 1,
+        key: 1
     },
     {
         letter: "A",
-        point: 1
+        point: 1,
+        key: 2
     },
     {
         letter: "B",
-        point: 3
+        point: 3,
+        key: 3
     },
     {
         letter: "C",
-        point: 3
+        point: 3,
+        key: 4
     },
     {
         letter: "C",
-        point: 3
+        point: 3,
+        key: 5
     },
     {
         letter: "D",
-        point: 2
+        point: 2,
+        key: 6
     },
     {
         letter: "D",
-        point: 2
+        point: 2,
+        key: 7
     },
     {
         letter: "D",
-        point: 2
+        point: 2,
+        key: 8
     },
     {
         letter: "E",
-        point: 1
+        point: 1,
+        key: 9
     },
     {
         letter: "E",
-        point: 1
+        point: 1,
+        key: 10
     },
     {
         letter: "E",
-        point: 1
+        point: 1,
+        key: 11
     },
     {
         letter: "F",
-        point: 4
+        point: 4,
+        key: 12
     },
     {
         letter: "F",
-        point: 4
+        point: 4,
+        key: 13
     },
     {
         letter: "G",
-        point: 2
+        point: 2,
+        key: 14
     },
     {
         letter: "G",
-        point: 2
+        point: 2,
+        key: 15
     },
     {
         letter: "G",
-        point: 2
+        point: 2,
+        key: 16
     },
     {
         letter: "H",
-        point: 4
+        point: 4,
+        key: 17
     },
     {
         letter: "H",
-        point: 4
+        point: 4,
+        key: 18
     },
     {
         letter: "I",
-        point: 1
+        point: 1,
+        key: 19
     },
     {
         letter: "I",
-        point: 1
+        point: 1,
+        key: 20
     },
     {
         letter: "I",
-        point: 1
+        point: 1,
+        key: 21
     },
     {
         letter: "J",
-        point: 8
+        point: 8,
+        key: 22
     },
     {
         letter: "K",
-        point: 5
+        point: 5,
+        key: 23
     },
     {
         letter: "L",
-        point: 1
+        point: 1,
+        key: 24
     },
     {
         letter: "L",
-        point: 1
+        point: 1,
+        key: 25
     },
     {
         letter: "L",
-        point: 1
+        point: 1,
+        key: 26
     },
     {
         letter: "M",
-        point: 3
+        point: 3,
+        key: 27
     },
     {
         letter: "M",
-        point: 3
+        point: 3,
+        key: 28
     },
     {
         letter: "N",
-        point: 1
+        point: 1,
+        key: 29
     },
     {
         letter: "N",
-        point: 1
+        point: 1,
+        key: 30
     },
     {
         letter: "N",
-        point: 1
+        point: 1,
+        key: 31
     },
     {
         letter: "O",
-        point: 1
+        point: 1,
+        key: 32
     },
     {
         letter: "O",
-        point: 1
+        point: 1,
+        key: 33
     },
     {
         letter: "O",
-        point: 1
+        point: 1,
+        key: 34
     },
     {
         letter: "P",
-        point: 3
+        point: 3,
+        key: 35
     },
     {
         letter: "P",
-        point: 3
+        point: 3,
+        key: 36
     },
     {
         letter: "Q",
-        point: 10
+        point: 10,
+        key: 37
     },
     {
         letter: "R",
-        point: 1
+        point: 1,
+        key: 38
     },
     {
         letter: "R",
-        point: 1
+        point: 1,
+        key: 39
     },
     {
         letter: "R",
-        point: 1
+        point: 1,
+        key: 40
     },
     {
         letter: "S",
-        point: 1
+        point: 1,
+        key: 41
     },
     {
         letter: "S",
-        point: 1
+        point: 1,
+        key: 42
     },
     {
         letter: "S",
-        point: 1
+        point: 1,
+        key: 43
     },
     {
         letter: "T",
-        point: 1
+        point: 1,
+        key: 44
     },
     {
         letter: "T",
-        point: 1
+        point: 1,
+        key: 45
     },
     {
         letter: "T",
-        point: 1
+        point: 1,
+        key: 46
     },
     {
         letter: "U",
-        point: 1
+        point: 1,
+        key: 47
     },
     {
         letter: "U",
-        point: 1
+        point: 1,
+        key: 48
     },
     {
         letter: "U",
-        point: 1
+        point: 1,
+        key: 49
     },
     {
         letter: "V",
-        point: 4
+        point: 4,
+        key: 50
     },
     {
         letter: "V",
-        point: 4
+        point: 4,
+        key: 51
     },
     {
         letter: "W",
-        point: 4
+        point: 4,
+        key: 52
     },
     {
         letter: "W",
-        point: 4
+        point: 4,
+        key: 53
     },
     {
         letter: "X",
-        point: 8
+        point: 8,
+        key: 54
     },
     {
         letter: "Y",
-        point: 4
+        point: 4,
+        key: 55
     },
     {
         letter: "Y",
-        point: 4
+        point: 4,
+        key: 56
     },
     {
         letter: "Z",
-        point: 10
+        point: 10,
+        key: 57
     },
 ]
 }
